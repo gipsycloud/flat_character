580a99931bcfc776d9a8a46cdb15e6617a82310e
aungwin <aungthiwin1801@gmail.com>
2023-11-14 00:40:36 +0630

    modified design in main page and adding new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :homes
+  get "list_a_room" => "homes#list_a_room"
+  get "find_a_home" => "homes#find_a_home"
+  get "find_a_flatmate" => "homes#find_a_flatmate"
   get "aboutus" => "homes#aboutus"
   get "services" => "homes#services"
   get "contact" => "homes#contact"
