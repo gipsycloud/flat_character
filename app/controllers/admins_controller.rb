49e8e8f2d299574d4349880bab0aa15563437589
aungwin <aungthiwin1801@gmail.com>
2023-11-20 17:24:36 +0630

    modified page in admin panel

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -62,6 +62,9 @@ class AdminsController < ApplicationController
   def report
   end
 
+  def homelist
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_admin
