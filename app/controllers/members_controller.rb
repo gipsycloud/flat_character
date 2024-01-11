cf20f51dd00c89a239721f53f68c793df2153b82
aungwin <aungthiwin1801@gmail.com>
2024-01-11 14:57:51 +0630

    adding member in admin panel:

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -65,6 +65,6 @@ class MembersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def member_params
-      params.fetch(:member, {})
+      params.require(:member).permit(:memberName, :email, :phoneNumber, :memberAddress, :joinDate, :lastLogin, :memberType)
     end
 end
