d688fd22cb979ce5fe33cb07a4949793649e9d4d
webgipsy <babybird1801@gmail.com>
2024-03-04 00:13:59 +0630

    update code for find_a_flatmate webpage

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,3 +1,12 @@
 class UserInfo < ApplicationRecord
   belongs_to :user
+
+  def description
+    if self.details.blank?
+      ""
+    else
+      detail = self.details
+      ActionView::Base.full_sanitizer.sanitize(detail).truncate(100)
+    end
+  end
 end
