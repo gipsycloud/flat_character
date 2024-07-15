c81061bbab7c58c63353444f9821be432fe549e1
webgipsy <babybird1801@gmail.com>
2024-07-15 15:28:10 +0630

    user authentication without token

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,47 +4,37 @@ class Api::V1::SessionsController < Devise::SessionsController
   respond_to :json
   # before_action :configure_sign_in_params, only: [:create]
 
+
   # GET /resource/sign_in
   # def new
   #   super
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    user = User.find_by_email(params[:email])
+    
+    if user && user.valid_password?(params[:password])
+      # byebug
+      payload = { user_id: user.id }
+      secret = ENV["SECRET_KEY_BASE"] || Rails.application.secrets.secret_key_base
+      @current_user = user
+      render json: { 
+        message: 'Logged in successfully.', 
+        user: @current_user,
+        # token: create_token(payload)
+      }, status: :ok
+    else
+      render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-
   private
-
-  def respond_with(resource, _opts = {})
-    render json: { 
-      message: 'Logged in successfully.', 
-      user: resource,
-      token: current_token
-    }, status: :ok
-  end
-
-  def respond_to_on_destroy
-    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise[:jwt_secret_key]).first
-    current_user = User.find(jwt_payload['sub'])
-    if current_user
-      render json: { message: 'Logged out successfully.' }, status: :ok
-    else
-      render json: { message: 'User not found.' }, status: :unauthorized
-    end
-  end
 
   def current_token
     request.env['warden-jwt_auth.token']
