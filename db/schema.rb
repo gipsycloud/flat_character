c5cf031f23ef1d502ecf527865a913cb3f51158b
webgipsy <babybird1801@gmail.com>
2024-09-06 00:20:15 +0630

    remove notify_ in subscribptions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_30_103535) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_05_174515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -138,7 +138,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_30_103535) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "notify_when_added_to_room", default: true
   end
 
   create_table "upgrades", force: :cascade do |t|
