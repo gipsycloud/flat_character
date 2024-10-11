ed685dd4266273b41760abe9c31ba135e44a501c
webgipsy <babybird1801@gmail.com>
2024-10-11 16:45:27 +0630

    merge hotfix to web/room_checkout

--- app/javascript/room_checkout.js
+++ app/javascript/room_checkout.js
@@ -1,15 +1,21 @@
-var room_price = document.getElementById('room_price').getAttribute('value');
-var service_fee = document.getElementById('service_fee').getAttribute('value');
-var sum = parseInt(room_price) + parseInt(service_fee);
+document.addEventListener("DOMContentLoaded", function() {
+  if (document.body.classList.contains('homes-room_detail')) {
+    var room_price = document.getElementById('room_price').getAttribute('value');
+    var service_fee = document.getElementById('service_fee').getAttribute('value');
+    var sum = parseInt(room_price) + parseInt(service_fee);
+    console.log(sum);
+    setInterval(function() {
+        document.getElementById('total').innerHTML = sum;
+        // location.reload();
+      }, 1000);
+    
+  }
+});
 
 // setTimeout(function() {
 //   document.getElementById('total').innerHTML = sum;
 //   // location.reload();
 // }, 1000);
-window.setInterval(function(e) {
-  document.getElementById('total').innerHTML = sum;
-  e.preventDefault();
-}, 1000);
 
 // b = parseFloat($('#b').val()).toFixed(2);
 // console.log('roomprice = ' + room_price, 'servicefee = ' + service_fee);
