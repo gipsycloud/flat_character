645d0fbef6ade03a8547a10be96a00c1d62bf2c4
webgipsy <babybird1801@gmail.com>
2024-10-11 13:32:08 +0630

    modify room checkout page with js

--- app/javascript/room_checkout.js
+++ app/javascript/room_checkout.js
@@ -1,8 +1,14 @@
 var room_price = document.getElementById('room_price').getAttribute('value');
 var service_fee = document.getElementById('service_fee').getAttribute('value');
 var sum = parseInt(room_price) + parseInt(service_fee);
-setInterval(function() {
+
+// setTimeout(function() {
+//   document.getElementById('total').innerHTML = sum;
+//   // location.reload();
+// }, 1000);
+window.setInterval(function(e) {
   document.getElementById('total').innerHTML = sum;
+  e.preventDefault();
 }, 1000);
 
 // b = parseFloat($('#b').val()).toFixed(2);
