46be48c7da7361c792423b2180bf553c6e6aecf7
aungwin <babybird1801@gmail.com>
2026-04-17 16:27:14 +0630

    update code for jquery error in render server

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w(application.js tailwind.css inter-font.css)
+Rails.application.config.assets.precompile += %w(application.js tailwind.css inter-font.css jquery.min.js jquery_ujs.js)
