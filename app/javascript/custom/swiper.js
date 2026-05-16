6320b7f5bd7017cbe542aa6a44fe0e718dd4dd7e
aungwin <babybird1801@gmail.com>
2026-05-16 20:14:19 +0630

    swiper js

--- app/javascript/custom/swiper.js
+++ app/javascript/custom/swiper.js
@@ -1,33 +1,28 @@
 // app/javascript/packs/swiper.js
 // https://swiperjs.com/get-started
 
-import swiper from 'https://cdn.jsdelivr.net/npm/swiper@11.0.3/+esm'
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
-// import Swiper from 'swiper';
-$(document).on("turbo:load", function() {
-// Initialize Swiper
-const mySwiper = new Swiper('.swiper', {
-  slidesPerView: 5,
-  spaceBetween: 15,
-  loop: true,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
-  autoHeight: 'true',
-  observer: true,  
-  observeParents: true,
-  keyboard: true,
-  // mousewheel: true,
-  autoplay: {
-    delay: 5000,
-  },
-  navigation: {
-    nextEl: '.button-next',
-    prevEl: '.button-prev',
-  },
-  // pagination: {
-  //   el: '.swiper-pagination',
-  //   clickable: true,
-  // },
-});
-});
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+
+document.addEventListener('turbo:load', () => {
+    // Initialize Swiper
+    new Swiper('.swiper', {
+      slidesPerView: 5,
+      spaceBetween: 15,
+      loop: true,
+      centeredSlides: true,
+      effect: 'fade',
+      grabCursor: true,
+      autoHeight: true,
+      observer: true,
+      observeParents: true,
+      keyboard: true,
+      autoplay: false,
+      autoplay: {
+        delay: 5000,
+      },
+      navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+      },
+    });
+  });
