4457a52cc2dad99237bf2b11e90313fdd22af050
aungwin <babybird1801@gmail.com>
2026-06-19 16:23:05 +0630

    modifing UI/UX for admin panel

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Blog
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.autoload_paths << Rails.root.join('app', 'services')
 
     config.assets.css_compressor = nil
 
