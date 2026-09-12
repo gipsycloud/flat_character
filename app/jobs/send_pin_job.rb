8eb04fed5df6b96ce70cb8ecb2804fdd1074b091
aungwin <babybird1801@gmail.com>
2026-09-12 11:13:53 +0630

    send pin job code fix

--- app/jobs/send_pin_job.rb
+++ app/jobs/send_pin_job.rb
@@ -1,9 +1,9 @@
 class SendPinJob < ApplicationJob
-  # queue_as :default
+  queue_as :default
 
   def perform(user)
-    user.reset_pin!                           # Reset the user's verification PIN
-    user.update(pin_sent_at: Time.now)        # Update the timestamp when the PIN was sent
-    UserMailer.send_pin(user).deliver_later     # Send the PIN email using the UserMailer
+    user.reset_pin!
+    user.update!(pin_sent_at: Time.current)
+    UserMailer.send_pin(user).deliver_now
   end
 end
